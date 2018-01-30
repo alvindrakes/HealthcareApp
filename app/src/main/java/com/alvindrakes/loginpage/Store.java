46ee79fc335c9ff55f124477a7b4f0eac933c92e
ming <ming2b@yahoo.com>
2018-01-30 10:58:35 +0800

    update store

--- app/src/main/java/com/alvindrakes/loginpage/Store.java
+++ app/src/main/java/com/alvindrakes/loginpage/Store.java
@@ -6,17 +6,21 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by ming on 24/1/2018.
  */
 
-public class Store extends AppCompatActivity {
+public class Store extends MainActivity{
+    TextView coin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.store);
+        coin = (TextView) findViewById(R.id.amount2);
 
         FloatingActionButton back = (FloatingActionButton) findViewById(R.id.close_store);
 
@@ -25,6 +29,53 @@ public class Store extends AppCompatActivity {
             public void onClick(View view) {
                 Intent StartPageIntent = new Intent(Store.this, MainActivity.class);
                 startActivity(StartPageIntent);
+            }
+        });
+        //user.getCoin();
+        //coin.setText(Integer.toString(user.getCoin()));
+        Button price_1 = (Button) findViewById(R.id.button2);
+        Button price_2 = (Button) findViewById(R.id.button3);
+        Button price_3 = (Button) findViewById(R.id.button4);
+        price_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              if (user.getCoin()>= 50) {
+                  user.setCoin(user.getCoin() - 50);
+                  coin.setText(Integer.toString(user.getCoin()));
+                  StatisticData.updateData(data,user.getDay(),user.getCoin());
+              }
+                else {
+                  Toast.makeText(Store.this, "Insufficient coins", Toast.LENGTH_SHORT).show();
+              }
+
+    }
+});
+        price_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (user.getCoin()>= 100) {
+                    user.setCoin(user.getCoin() - 100);
+                    coin.setText(Integer.toString(user.getCoin()));
+                    StatisticData.updateData(data,user.getDay(),user.getCoin());
+                }
+                else {
+                    Toast.makeText(Store.this, "Insufficient coins", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+        price_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (user.getCoin()>= 200) {
+                    user.setCoin(user.getCoin() - 200);
+                    coin.setText(Integer.toString(user.getCoin()));
+                    StatisticData.updateData(data,user.getDay(),user.getCoin());
+                }
+                else {
+                    Toast.makeText(Store.this, "Insufficient coins", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
     }
