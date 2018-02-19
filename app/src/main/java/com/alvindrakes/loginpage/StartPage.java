14a1f38a08fede9b89368d92ed6d0602cc032cfe
superlws1 <superlws1@hotmail.com>
2018-02-19 19:35:23 +0800

    sign in fixed google

--- app/src/main/java/com/alvindrakes/loginpage/StartPage.java
+++ app/src/main/java/com/alvindrakes/loginpage/StartPage.java
@@ -91,6 +91,7 @@ public class StartPage extends AppCompatActivity {
       } catch (ApiException e) {
         // Google Sign In failed, update UI appropriately
         Log.w(TAG, "Google sign in failed", e);
+        Toast.makeText(this,"Google sign in failed",Toast.LENGTH_SHORT).show();
         // ...
       }
     }
