8c0006255071161ac38e809ea199cd5ccd408752
ming <ming2b@yahoo.com>
2018-02-22 11:56:18 +0800

    home navigation bar appear again

--- app/src/main/java/com/alvindrakes/loginpage/MainActivity.java
+++ app/src/main/java/com/alvindrakes/loginpage/MainActivity.java
@@ -104,6 +104,7 @@ public class MainActivity extends AppCompatActivity implements OnDataPointListen
 
         myDrawer.addDrawerListener(myToggle);
         myToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         ActionBar bar = getSupportActionBar();
         bar.setTitle("Homepage");
         getSupportActionBar().show();
