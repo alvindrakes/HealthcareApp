7a7954afde18a303c7ce05857247832b57aeb0a7
alvindrakes <alvinlohjinxian@gmail.com>
2018-03-07 18:57:10 +0800

    Fix memory problem

--- app/src/main/java/com/alvindrakes/loginpage/MainActivity.java
+++ app/src/main/java/com/alvindrakes/loginpage/MainActivity.java
@@ -4,6 +4,8 @@ package com.alvindrakes.loginpage;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.hardware.Sensor;
@@ -283,5 +285,4 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         startActivity(intent);
         System.out.print("Game is running !!!!!!!");
     }
-
 }
